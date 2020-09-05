4d45ae9c13326d12d6e849afd753d9a247f7763a
amaljoe <amaljoe88@gmail.com>
2020-09-05 13:27:29 +0530

    Implemented ListView builder for song menu and song display also using sql database

--- lib/screens/song_display.dart
+++ lib/screens/song_display.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:songbook_flutter/constants.dart';
 import 'package:songbook_flutter/components/song_toolbar.dart';
 import 'package:songbook_flutter/song_item_manager.dart';
+import 'package:songbook_flutter/songs_database.dart';
+
+import '../song_item.dart';
 
 class SongDisplay extends StatefulWidget {
   final int songNum;
@@ -11,30 +14,45 @@ class SongDisplay extends StatefulWidget {
 }
 
 class _SongDisplayState extends State<SongDisplay> {
+  SongsDatabase songsDatabase = SongsDatabase();
   SongItemManager songItemManager = SongItemManager();
+
+  Future<List<SongItem>> getSongs() async {
+    await songsDatabase.openSongsDatabase();
+    return songsDatabase.getAllSongs();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Stack(children: [
-          Container(
-            padding:
-                EdgeInsets.only(top: kSongToolbarHeight - kToolbarBorderRadius),
-            color: Colors.green,
-            child: Container(
-              height: double.infinity,
-              width: double.infinity,
-              color: Colors.white,
-              child: Padding(
-                padding: EdgeInsets.only(
-                    top: kToolbarBorderRadius + 8, left: 8, right: 8),
-                child: Text(
-                  songItemManager.getSong(widget.songNum).lyrics,
-                  style: kSongLyricsTextStyle,
-                ),
-              ),
-            ),
-          ),
+          FutureBuilder(
+              future: getSongs(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Container(
+                    padding: EdgeInsets.only(
+                        top: kSongToolbarHeight - kToolbarBorderRadius),
+                    color: Colors.green,
+                    child: Container(
+                      height: double.infinity,
+                      width: double.infinity,
+                      color: Colors.white,
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                            top: kToolbarBorderRadius + 8, left: 8, right: 8),
+                        child: Text(
+                          snapshot.data[widget.songNum].lyrics,
+                          style: kSongLyricsTextStyle,
+                        ),
+                      ),
+                    ),
+                  );
+                } else {
+                  return Container();
+                }
+              }),
           SongToolbar(
             navigationIcon: Icons.arrow_back,
             onIconPressed: () {
