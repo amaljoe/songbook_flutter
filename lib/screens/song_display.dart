262b8666fe33bbc7861f1e85e851f4151466c466
Amal Joe R S <amaljoe88@gmail.com>
2020-09-14 08:43:27 +0530

    Song display title showing correct title

--- lib/screens/song_display.dart
+++ lib/screens/song_display.dart
@@ -5,7 +5,6 @@ import 'package:songbook_flutter/songs_database.dart';
 
 import '../components/song_item_widget.dart';
 import '../song_item.dart';
-import '../song_item.dart';
 
 class SongDisplay extends StatefulWidget {
   final int songNum;
@@ -25,49 +24,47 @@ class _SongDisplayState extends State<SongDisplay> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(children: [
-          FutureBuilder(
-              future: getSongs(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return Container(
-                    padding: EdgeInsets.only(
-                        top: kSongToolbarHeight - kToolbarBorderRadius),
-                    color: Colors.green,
-                    child: Container(
-                      height: double.infinity,
-                      width: double.infinity,
-                      color: Colors.white,
-                      child: Padding(
+        body: SafeArea(
+            child: FutureBuilder(
+                future: getSongs(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Stack(children: [
+                      Container(
                         padding: EdgeInsets.only(
-                            top: kToolbarBorderRadius + 8, left: 8, right: 8),
-                        child: Text(
-                          snapshot.data[widget.songNum].lyrics,
-                          style: kSongLyricsTextStyle,
+                            top: kSongToolbarHeight - kToolbarBorderRadius),
+                        color: Colors.green,
+                        child: Container(
+                          height: double.infinity,
+                          width: double.infinity,
+                          color: Colors.white,
+                          child: Padding(
+                            padding: EdgeInsets.only(
+                                top: kToolbarBorderRadius + 8,
+                                left: 8,
+                                right: 8),
+                            child: Text(
+                              snapshot.data[widget.songNum].lyrics,
+                              style: kSongLyricsTextStyle,
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                } else {
-                  return Container();
-                }
-              }),
-          SongToolbar(
-            navigationIcon: Icons.arrow_back,
-            onIconPressed: () {
-              Navigator.pop(context);
-            },
-            childHeader: Center(
-              child: SongItemWidget(
-                  songItem: SongItem(
-                      title: 'നിന്‍റെ ഹിതം പോലെയെന്നെ നിത്യം നടത്തീടേണമേ',
-                      songId: 258),
-                  onPressed: () {}),
-            ),
-          ),
-        ]),
-      ),
-    );
+                      SongToolbar(
+                        navigationIcon: Icons.arrow_back,
+                        onIconPressed: () {
+                          Navigator.pop(context);
+                        },
+                        childHeader: Center(
+                          child: SongItemWidget(
+                              songItem: snapshot.data[widget.songNum],
+                              onPressed: () {}),
+                        ),
+                      ),
+                    ]);
+                  } else {
+                    return Container();
+                  }
+                })));
   }
 }
