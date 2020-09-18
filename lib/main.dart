b4abcf5706eb6fce4a854396dc7c7371989e1f62
Amal Joe R S <amaljoe88@gmail.com>
2020-09-18 11:23:25 +0530

    Removed transition to song display screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:songbook_flutter/screens/song_display.dart';
 import 'package:songbook_flutter/screens/song_menu.dart';
@@ -19,9 +18,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       initialRoute: SongMenu.id,
       theme: ThemeData.light(),
+      onGenerateRoute: (settings) {
+        if (settings.name == SongDisplay.id) {
+          return PageRouteBuilder(
+            pageBuilder: (_, __, ___) {
+              return SongDisplay();
+            },
+            transitionDuration: Duration(seconds: 0),
+            transitionsBuilder: (_, animation, __, child) {
+              var begin = Offset(0.0, 1.0);
+              var end = Offset.zero;
+              var curve = Curves.ease;
+
+              var tween = Tween(begin: begin, end: end);
+              var curvedAnimation = CurvedAnimation(
+                parent: animation,
+                curve: curve,
+              );
+
+              return child;
+            },
+          );
+        }
+        return null;
+      },
       routes: {
         SongMenu.id: (context) => SongMenu(),
-        SongDisplay.id: (context) => SongDisplay(),
         SongSearch.id: (context) => SongSearch(),
       },
     );
