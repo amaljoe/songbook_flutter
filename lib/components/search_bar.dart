6320506919755ddc2019198109fa0ab18a1a55f7
Amal Joe R S <amaljoe88@gmail.com>
2020-09-20 11:13:10 +0530

    Fine tune search bar - border size to 1.5

--- lib/components/search_bar.dart
+++ lib/components/search_bar.dart
@@ -17,7 +17,7 @@ class SearchBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        border: Border.all(width: 2),
+        border: Border.all(width: 1.5),
         borderRadius: BorderRadius.circular(40.0),
       ),
       child: GestureDetector(
