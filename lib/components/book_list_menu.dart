e6ee60cf14273578dc60873541159e8a4c90e579
amaljoe <amaljoe88@gmail.com>
2026-03-02 00:48:56 +0530

    add scrollbar

--- lib/components/book_list_menu.dart
+++ lib/components/book_list_menu.dart
@@ -3,26 +3,51 @@ import 'package:provider/provider.dart';
 import 'package:songbook_flutter/components/book_item_widget.dart';
 import 'package:songbook_flutter/models/book_data.dart';
 
-class BookListMenu extends StatelessWidget {
+class BookListMenu extends StatefulWidget {
   final Function onPressed;
 
   BookListMenu({required this.onPressed});
 
   @override
+  State<BookListMenu> createState() => _BookListMenuState();
+}
+
+class _BookListMenuState extends State<BookListMenu> {
+  late final ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scrollbar(
+      controller: _scrollController,
+      thumbVisibility: true,
+      trackVisibility: true,
+      interactive: true,
       child: ListView.builder(
-      itemCount: context.select<BookData, int>((value) => value.pages?.length ?? 0),
-      itemBuilder: (context, index) {
-        return GestureDetector(
-          onTap: () {
-            onPressed(index);
-          },
-          child: BookItemWidget(
-            bookItem: context.read<BookData>().pages![index],
-          ),
-        );
-      },
+        controller: _scrollController,
+        itemCount:
+            context.select<BookData, int>((value) => value.pages?.length ?? 0),
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              widget.onPressed(index);
+            },
+            child: BookItemWidget(
+              bookItem: context.read<BookData>().pages![index],
+            ),
+          );
+        },
       ),
     );
   }
--- lib/components/song_list_menu.dart
+++ lib/components/song_list_menu.dart
@@ -3,26 +3,51 @@ import 'package:provider/provider.dart';
 import 'package:songbook_flutter/components/song_item_widget.dart';
 import 'package:songbook_flutter/models/song_data.dart';
 
-class SongListMenu extends StatelessWidget {
+class SongListMenu extends StatefulWidget {
   final Function onPressed;
 
   SongListMenu({required this.onPressed});
 
   @override
+  State<SongListMenu> createState() => _SongListMenuState();
+}
+
+class _SongListMenuState extends State<SongListMenu> {
+  late final ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scrollbar(
+      controller: _scrollController,
+      thumbVisibility: true,
+      trackVisibility: true,
+      interactive: true,
       child: ListView.builder(
-      itemCount: context.select<SongData, int>((value) => value.songs?.length ?? 0),
-      itemBuilder: (context, index) {
-        return GestureDetector(
-          onTap: () {
-            onPressed(index);
-          },
-          child: SongItemWidget(
-            songItem: context.read<SongData>().songs![index],
-          ),
-        );
-      },
+        controller: _scrollController,
+        itemCount:
+            context.select<SongData, int>((value) => value.songs?.length ?? 0),
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              widget.onPressed(index);
+            },
+            child: SongItemWidget(
+              songItem: context.read<SongData>().songs![index],
+            ),
+          );
+        },
       ),
     );
   }
